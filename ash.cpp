f234628851aeb0af35472a4dc6ed3649d8be4872
James Ostmann <jsostmann@aggies.ncat.edu>
2020-09-26 17:53:23 -0400

    fixed parsing

--- ash.cpp
+++ ash.cpp
@@ -5,48 +5,51 @@
 #include <cstdlib>
 #include <cstdio>
 
-#define ESC "\e[0m"
-#define CUSTOM "\e[33;1ma\e[0m\e[34;1ms\e[0m\e[33;1mh\e[0m> "
+#define PROMPT "\e[33;1ma\e[0m\e[34;1ms\e[0m\e[33;1mh\e[0m>"
 
 using namespace std;
 
 void exit() {
-    cout << "Thank you for using our aggie shell" << endl;
-    exit(0);
+  cout << "Thank you for using our aggie shell" << endl;
+  cout << "Made By: " << "Joshua Grovan, & James Ostmann" << endl;
+  exit(0);
 }
 
-string readInput() {
-    stringstream buffer;
-    bool spaceToogle = false;
-    while (true) {
-        char c = cin.get();
-        
-        if (c == '\n') break;
-        
-        if (!isspace(c) ||(isspace(c) && !spaceToogle))
-            buffer << c;
-        if (isspace(c))
-            spaceToogle = true;
-        else
-            spaceToogle = false;
-    }
-    
-    return buffer.str();
+string getInput(string answer) {
+  getline(cin, answer);
+  return answer;
 }
 
-int main() {
-    
-    string userInput;
-    
-    while (true) {
-        cout << CUSTOM;
+// TODO: implement parser for individual commmands
+void parseCommands(string userInput){
+  string parse;
+  bool space = false;
+  for(int i = 0; i < userInput.length(); i++) {
         
-        userInput = readInput();
-        
-        if(userInput == "exit") {
-            exit();
-        }
-        
+    if(space && isspace(userInput.at(i))) {
+      space = false;
     }
-    return 0;
+    if(isspace(userInput.at(i))) {
+      space = true;
+    }
+    if(space) {
+      parse += userInput.at(i);
+    }
+  }
+  cout << parse << parse.length() << endl;
+}
+
+int main(int argv, char** argc) {
+  string userInput;
+    
+  while (true) {
+    cout << PROMPT;
+    userInput = getInput(userInput);
+    
+    if(userInput == "exit") {
+      exit();
+    }
+        
+  }
+  return 0;
 }
