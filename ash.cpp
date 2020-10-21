6d6e346626206b1d5ca2cbe424cc344d73847099
James Ostmann <jsostmann@aggies.ncat.edu>
2020-10-21 11:25:10 -0400

    added cd and pwd

--- ash.cpp
+++ ash.cpp
@@ -1,33 +1,48 @@
 /*
- *  James Ostmann & Joshua Govan
- *  COMP 350 / 001
- *  Sep, 26th 2020
- *  Ash Shell Project Checkpoint 1
+  * James Ostmann & Joshua Govan
+  * COMP 350 / 001
+  * Sep, 26th 2020
+  * Ash Shell Project Checkpoint 1
 */
-
 #include <iostream>
 #include <string>
 #include <cctype>
 #include <sstream>
 #include <cstdlib>
 #include <cstdio>
+#include <cstring>
+#include <unistd.h>
 
 #define ESC "\e[0m"
 #define AUTHOR "\e[40;38;5;82;4m"
 #define ESC_AUTHOR "\e[0;24m"
 #define PROMPT "\e[33;1ma\e[0m\e[34;1ms\e[0m\e[33;1mh\e[0m>"
-
+char promp_message[10] = "whoosh> ";
 using namespace std;
 
+// gets the string value of the current working directory with max length allocated to 100 chars
+string get_working_directory() {
+  char * directory = NULL;
+  return string(getcwd(directory,500));
+}
+
+// exit function called when user enters exit command
 void exit() {
   cout << "Thank you for using our Aggie Shell!" << endl;
   cout << "Made By: " << AUTHOR << "Joshua Govan"<< ESC_AUTHOR << " & " << AUTHOR << "James Ostmann" << ESC_AUTHOR << endl;
   exit(0);
 }
 
+// function to get userinput when entered from cin
 string getInput(string answer) {
   getline(cin, answer);
   return answer;
+}
+
+// error function to handle all errors that are encountered
+void error() {
+  write(STDERR_FILENO, promp_message, strlen(promp_message));
+  exit();
 }
 
 // TODO: implement parser for individual commmands
@@ -35,7 +50,6 @@ void parseCommands(string userInput){
   string parse;
   bool space = false;
   for(int i = 0; i < userInput.length(); i++) {
-        
     if(space && isspace(userInput.at(i))) {
       space = false;
     }
@@ -49,17 +63,27 @@ void parseCommands(string userInput){
   cout << parse << parse.length() << endl;
 }
 
+
 int main(int argv, char** argc) {
+
   string userInput;
-    
+
   while (true) {
-    cout << PROMPT;
-    userInput = getInput(userInput);
+
     
-    if(userInput == "exit") {
+    cout << get_working_directory() << " " << PROMPT;
+    
+    userInput = getInput(userInput);
+
+    if(userInput == "") {
+
+    } else if(userInput == "cd") {
+      cout << chdir("..") << endl;
+    } else if(userInput == "exit") {
       exit();
     }
-        
+
   }
+
   return 0;
 }
